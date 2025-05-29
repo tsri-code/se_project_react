eaf76066c5fdee27d692d0d3ab2a13074c687034
Sridhar Tiwari <tiwari4002@outlook.com>
2025-05-29 00:37:46 -0400

    Header Structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
+import "./Header.css";
+import logo from "../../assets/main-logo.svg";
+import user from "../../assets/user-logo.svg";
+
 function Header() {
   return (
     <header className="header">
-      <div className="header__content">Header</div>
+      <div className="header__left">
+        <img src={logo} alt="Logo" className="header__logo" />
+        <p className="header__date-and-location">Date and location</p>
+      </div>
+      <div className="header__right">
+        <button className="header__button">+ Add Clothes</button>
+        <div className="header__user-container">
+          <p className="header__user-name">Terence Tegegne</p>
+          <img src={user} alt="Terence Tegegne" className="header__avatar" />
+        </div>
+      </div>
     </header>
   );
 }
