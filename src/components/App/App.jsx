7ae4b5ba0c1ea71526880eca2f38372cf5b2aa51
Sridhar Tiwari <tiwari4002@outlook.com>
2025-05-30 21:57:57 -0400

    Card Structure and Setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
+import "./Main.css";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
         <p>Today is 75 &deg; F / You may want to wear:</p>
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => (
+              <ItemCard key={item._id} item={item} />
+            ))}
+        </ul>
       </section>
     </main>
   );
